55400cd4170c890ccc8efcf5972b5d92928a7c77
thiagohcc <dzno23@gmail.com>
2023-09-15 00:03:11 -0300

    FIX: Corrige o nome da model na seeder users

--- backend/src/database/seeders/users.js
+++ backend/src/database/seeders/users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('users', [
+    await queryInterface.bulkInsert('user', [
       {
         name: 'admin',
         email: 'admin@admin.com',
@@ -14,6 +14,6 @@ module.exports = {
 
 
   down: async (queryInterface) => {
-    await queryInterface.bulkDelete('users', null, {});
+    await queryInterface.bulkDelete('user', null, {});
   },
 };
