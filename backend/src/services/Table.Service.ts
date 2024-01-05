5c79e0a2014abb1890d00e0be95938186f3a7b21
thiagohcc <dzno23@gmail.com>
2024-01-05 14:24:43 -0300

    FEAT: Cria a service de Table

---
+++ backend/src/services/Table.Service.ts
@@ -0,0 +1,28 @@
+import Table from '../database/models/Table.Model';
+
+export default class CreateTableService {
+  getAll = async () => {
+    try{
+      const tables = await Table.findAll();
+      return { type: 200, data: tables }
+    } catch (err) {
+      return { type: 500, message: (err as Error).message }
+    }
+  }
+  getOne = async (id: number) => {
+    try{
+      const table = await Table.findByPk(id);
+      return { type: 200, data: table }
+    } catch (err) {
+      return { type: 500, message: (err as Error).message }
+    }
+  }
+  getAllUnoccupied = async () => {
+    try{
+      const tables = await Table.findAll({ where: { isOccupied: false } });
+      return { type: 200, data: tables }
+    } catch (err) {
+      return { type: 500, message: (err as Error).message }
+    }
+  }
+}
