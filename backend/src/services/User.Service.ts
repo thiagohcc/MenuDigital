4c5cfa041d816581110249c88ac6e2c5a88342b1
thiagohcc <dzno23@gmail.com>
2024-01-05 14:57:15 -0300

    FEAT: Aprimora a service User

--- backend/src/services/User.Service.ts
+++ backend/src/services/User.Service.ts
@@ -33,4 +33,34 @@ export default class UserService {
       return { type: 500, message: (err as Error).message }
     }
   }
+  post = async (user: User) => {
+    try {
+      const userExists = await User.findOne({ where: { email: user.email } });
+      if (user.email == userExists?.email) return { type: 401, message: 'Email já cadastrado!' }
+      const newUser = await User.create(user);
+      return { type: 200, data: newUser }
+    } catch (err) {
+      return { type: 500, message: (err as Error).message }
+    }
+  }
+
+  put = async (id: number, user: User) => {
+    try {
+      const updatedUser = await User.update(user, { where: { id } });
+      return { type: 200, data: updatedUser }
+    } catch (err) {
+      return { type: 500, message: (err as Error).message }
+    }
+  }
+
+  delete = async (id: number) => {
+    try {
+      const user = await User.findByPk(id);
+      if (!user) return { type: 404, message: 'Usuário não encontrado' }
+      await user.destroy();
+      return { type: 200, message: 'Usuário deletado' }
+    } catch (err) {
+      return { type: 500, message: (err as Error).message }
+    }
+  }
 }
